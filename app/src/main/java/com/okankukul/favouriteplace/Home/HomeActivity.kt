fe488419f719081cf6ef2f24ccce3ea1b57c8400
okankkl <=>
2022-06-08 18:10:06 +0300

    Ana sayfaya menü eklendi

--- app/src/main/java/com/okankukul/favouriteplace/Home/HomeActivity.kt
+++ app/src/main/java/com/okankukul/favouriteplace/Home/HomeActivity.kt
@@ -3,10 +3,14 @@ package com.okankukul.favouriteplace.Home
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import com.okankukul.favouriteplace.Adapter.RecylerAdapter
 import com.okankukul.favouriteplace.Add.AddActivity
+import com.okankukul.favouriteplace.Login_Register.LoginActivity
 import com.okankukul.favouriteplace.Model.Post
 import com.okankukul.favouriteplace.R
 import com.okankukul.favouriteplace.databinding.ActivityHomeBinding
@@ -14,6 +18,7 @@ import com.okankukul.favouriteplace.databinding.ActivityHomeBinding
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityHomeBinding
+    private lateinit var auth : FirebaseAuth
     private lateinit var recyclerViewAdapter : RecylerAdapter
 
     var postList = ArrayList<Post>()
@@ -25,15 +30,38 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnGoAdd.setOnClickListener {
-            startActivity(Intent(applicationContext,AddActivity::class.java))
-        }
+        auth = FirebaseAuth.getInstance()
+
+
 
         val layoutManager = LinearLayoutManager(this)
         binding.recylerView.layoutManager = layoutManager
         recyclerViewAdapter= RecylerAdapter(postList)
         binding.recylerView.adapter = recyclerViewAdapter
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout)
+        {
+            auth.signOut()
+            startActivity(Intent(applicationContext,LoginActivity::class.java))
+            finish()
+
+        }
+        else if (item.itemId == R.id.addPlace)
+        {
+            startActivity(Intent(applicationContext,AddActivity::class.java))
+        }
+
+
+        return super.onOptionsItemSelected(item)
     }
 
 
