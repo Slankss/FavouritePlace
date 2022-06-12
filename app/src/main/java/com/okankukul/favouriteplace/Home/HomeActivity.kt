a32a8e2985679086b6dbd76257950666073cea92
okankkl <=>
2022-06-12 15:40:20 +0300

    addFragment sorunlar var

--- app/src/main/java/com/okankukul/favouriteplace/Home/HomeActivity.kt
+++ app/src/main/java/com/okankukul/favouriteplace/Home/HomeActivity.kt
@@ -1,24 +1,10 @@
 package com.okankukul.favouriteplace.Home
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
-import com.okankukul.favouriteplace.Adapter.RecylerAdapter
-import com.okankukul.favouriteplace.Login_Register.LoginActivity
-import com.okankukul.favouriteplace.Model.Post
 import com.okankukul.favouriteplace.R
 import com.okankukul.favouriteplace.databinding.ActivityHomeBinding
 import javax.annotation.meta.When
@@ -43,7 +29,7 @@ class HomeActivity : AppCompatActivity() {
         bottom.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.home -> loadFragment(HomeFragment())
-                R.id.add -> loadFragment(AddFragment())
+                R.id.add -> loadFragment(AddFragment(this))
             }
             true
         }
